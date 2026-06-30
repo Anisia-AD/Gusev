b97a42d8aa4a86173fbf2f9bcd124edb85c8bf71
A.D. <podkatilovaanisia@gmail.com>
2026-07-01 00:39:23 +0300

    Update settings.py

--- legal_project/settings.py
+++ legal_project/settings.py
@@ -1,9 +1,12 @@
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-your-secret-key-here-change-in-production'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = True
 
