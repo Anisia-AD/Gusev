96179fd08af2f4ed4c729cbdb9bf3f3590740c98
A.D. <podkatilovaanisia@gmail.com>
2026-07-06 15:34:07 +0300

    Update admin.py

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Appointment
 
-# Register your models here.
+@admin.register(Appointment)
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'phone', 'created_at', 'is_processed')
+    list_filter = ('is_processed', 'created_at')
+    search_fields = ('name', 'phone', 'problem')
+    actions = ['mark_as_processed']
+
+    def mark_as_processed(self, request, queryset):
+        queryset.update(is_processed=True)
+    mark_as_processed.short_description = 'Отметить как обработанные'
