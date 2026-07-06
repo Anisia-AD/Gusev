1c96bf92acc0bf915ec6b820da2cd2bc91b053ef
A.D. <podkatilovaanisia@gmail.com>
2026-07-06 22:56:43 +0300

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,13 +1,9 @@
 from django.urls import path
 from . import views
 
-print("✅ Загрузка core/urls.py...")
-
 urlpatterns = [
     path('', views.index, name='index'),
     path('services/', views.services, name='services'),
     path('contacts/', views.contacts, name='contacts'),
     path('appointment/', views.appointment, name='appointment'),
 ]
-
-print("✅ Маршруты core загружены!")
