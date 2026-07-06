29f7d9ba2bc83c540ad4b0f7b57bc26debbc7057
A.D. <podkatilovaanisia@gmail.com>
2026-07-06 15:33:49 +0300

    Update models.py

--- core/models.py
+++ core/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Appointment(models.Model):
+    name = models.CharField('ФИО', max_length=200)
+    phone = models.CharField('Телефон', max_length=20)
+    problem = models.TextField('Проблема')
+    created_at = models.DateTimeField('Дата создания', auto_now_add=True)
+    is_processed = models.BooleanField('Обработано', default=False)
+
+    def __str__(self):
+        return f'{self.name} - {self.phone}'
+
+    class Meta:
+        verbose_name = 'Заявка'
+        verbose_name_plural = 'Заявки'
+        ordering = ['-created_at']
